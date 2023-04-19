40d38f4112f05c05eca0430cce8327168d43ee76
Hasan Atki <djamblers@gmail.com>
2023-04-19 22:16:22 +0000

    Unresolved: hostname/ip does not match certificate's altnames

--- routes/auth.js
+++ routes/auth.js
@@ -3,30 +3,22 @@ const User = require('../models/User')
 const CryptoJS = require('crypto-js')
 
 // Register
-router.get('/signup', (req, res) => res.send('Successful'))
-
 router.post('/signup', async (req, res) => {
-
-    // --add validation 
+    // to add validation 
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
         password: CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString(),
     })
-
     try {
         const savedUser = await newUser.save()
-        console.log('Created User')
         res.status(201).json(savedUser)
-        res.send('Successful')
     } catch(err) {
-        console.log('Didnt create User')
         res.status(500).json(err)
     }
 })
 
 // Login
-
 router.post('/login', async (req, res) => {
     try {
         const user = await User.findOne({ username: req.body.username }) 
